d70a7c96cf29559e2a8deadb371681dfa85b061a
ranisharafeeque <ranisharafeeque@gmail.com>
2021-06-16 20:01:13 -0400

    styling

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,7 +4,8 @@ import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
-const logo = "/assets/Pollr_Text_White.png";
+//const logo = "/assets/Pollr_Text_White.png";
+const logo = "/assets/oie_OakRVG2UnYoK.jpg";
 
 render(
   //<App socket={socket}/>,
